31b1940eed453763ca454f0e0f9926da50485439
Wesley Santos <wesley.trindade.santos@gmail.com>
2013-11-26 00:09:49 -0200

    Teste
    ....

---
+++ login.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>Login Web Software</title>
+		 <link rel="stylesheet" href="css/bootstrap.css"/>
+		
+		
+
+		</style>
+	</head>
+		<body>
+		
+			<form name="formlogin" method="POST" action="userautentication.php">
+				<div id="envelope">
+					<div class="panel panel-default">
+					 <div class="panel-heading"><div id="cab">Web-Software(Eco IT)</div></div>
+						<div class="panel-body">
+							Log-in <input type="text" class="form-control" name="email" placeholder="E-mail" /> </br></br>
+							Senha  <input type="password"  class="form-control" name="senha" placeholder="Senha" /> </br></br>
+							<input id="btlog" type="Submit" class="btn btn-default" value="Entrar" />
+						</div>
+						<div class="panel-footer">
+						<p>Desenvolvido por Eco IT  &#169 </p>
+						</div>
+					</div>
+				</div>
+			</form>
+		
+		
+		</body>
+</html>
